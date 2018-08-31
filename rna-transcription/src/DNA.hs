d6ef3c80f8c5b6dfa110f0550d8d9c1bc5f7eefb
Milad Bourhani <milad.bourhani@gmail.com>
2018-08-31 23:05:06 +0200

    improved

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,19 +1,15 @@
 module DNA (toRNA) where
 
-import Control.Applicative
-
 toRNA :: String -> Maybe String
-toRNA xs = reverse <$> (foldr connect (Just "") $ fmap compl xs)
-
-connect :: Maybe Char -> Maybe String -> Maybe String
-connect xs c = append <$> xs <*> c
-
-append :: Char -> String -> String
-append c xs = xs ++ [c]
+toRNA = sequence . fmap compl
 
 compl :: Char -> Maybe Char
-compl 'G' = Just 'C'
-compl 'C' = Just 'G'
-compl 'T' = Just 'A'
-compl 'A' = Just 'U'
-compl _   = Nothing
+compl = flip lookup transcription
+
+transcription :: [(Char,Char)]
+transcription = [
+    ('G', 'C'),
+    ('C', 'G'),
+    ('T', 'A'),
+    ('A', 'U')
+  ]

a5fc3224cc8c7e01bbd8dfe295c33746e4b313c9
milad.bourhani <milad.bourhani@accenture.com>
2018-09-10 16:04:16 +0200

    used uncurry

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -3,5 +3,5 @@ module Hamming (distance) where
 distance :: String -> String -> Maybe Int
 distance xs ys
   | length xs /= length ys = Nothing
-  | otherwise = Just $ length $ filter (\(f,s) -> f /= s) $ zip xs ys
+  | otherwise = Just $ length $ filter (uncurry (/=)) $ zip xs ys
 

8df6eae3de47f0d586ea1699b66e70f85cb28964
milad.bourhani <milad.bourhani@accenture.com>
2018-09-19 10:05:39 +0200

    following advice from mentor

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -3,9 +3,9 @@ module Grains (square, total) where
 import Control.Monad
 
 square :: Integer -> Maybe Integer
-square n | n > 0 && n <= 64 = Just . (2^) . (subtract 1) $ n
+square n | n > 0 && n <= 64 = Just . (2^) $ n - 1
          | otherwise        = Nothing
 
 total :: Integer
-total = sum $ fmap (2^) . fmap (subtract 1) $ [1..64]
+total = sum $ fmap (2^) $ [0..63]
 

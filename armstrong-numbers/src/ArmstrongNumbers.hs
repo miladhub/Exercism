be2a486bcf2c0d578abafa51c557a2d72103301f
Milad Bourhani <milad.bourhani@gmail.com>
2018-11-24 22:58:17 +0100

    armstrong

--- armstrong-numbers/src/ArmstrongNumbers.hs
+++ armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,15 @@
 module ArmstrongNumbers (armstrong) where
 
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong a =
+  let n = length . digits $ a
+  in (== a) . sum . fmap (^n) . digits $ a
+
+digits :: Integral a => a -> [a]
+digits a =
+  let first = a `mod` 10
+  in
+    if first == a then
+      [first]
+    else
+      digits (a `div` 10) ++ [first]

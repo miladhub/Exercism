69121dd7d72183bd62f3943799b12655445381c6
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-11 22:35:55 +0200

    improved

--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -2,7 +2,9 @@ module Connect (Mark(..), winner) where
 
 import Control.Applicative
 
-data Mark = Cross | Nought deriving (Eq, Show)
+data Mark =
+  Cross | Nought
+  deriving (Eq, Show)
 
 winner :: [String] -> Maybe Mark
 winner board =
@@ -17,19 +19,19 @@ pathsFromStart board Cross  = [find board [] 'X' (r, 0) | r <- [0..(lastRow boar
 pathsFromStart board Nought = [find board [] 'O' (0, c) | c <- [0..(lastCol board)]]
 
 find :: [String] -> [(Int,Int)] -> Char -> (Int, Int) -> Maybe Mark
-find b e ch rc =
-  let (r,c) = rc
-      outOfBounds b r c = r > lastRow b || c > lastCol b || r < 0 || c < 0
+find board checkedCells char cell =
+  let (row, col) = cell
+      outOfBounds board row col = row > lastRow board || col > lastCol board || row < 0 || col < 0
       alreadyChecked = elem
-      noMatch b r c = (/=) (b !! r !! c)
+      noMatch board row col = (/=) (board !! row !! col)
   in
-    if outOfBounds b r c || alreadyChecked rc e || noMatch b r c ch then Nothing
-    else if ch == 'X' && c == lastCol b then Just Cross
-    else if ch == 'O' && r == lastRow b then Just Nought
+    if outOfBounds board row col || alreadyChecked cell checkedCells || noMatch board row col char then Nothing
+    else if char == 'X' && col == lastCol board then Just Cross
+    else if char == 'O' && row == lastRow board then Just Nought
     else
       foldl (<|>) Nothing $
-        (find b (rc : e) ch) <$>
-          [(r - 1, c), (r + 1, c), (r, c + 1), (r, c - 1), (r + 1, c - 1), (r - 1, c + 1)]
+        (find board (cell : checkedCells) char) <$>
+          [(row - 1, col), (row + 1, col), (row, col + 1), (row, col - 1), (row + 1, col - 1), (row - 1, col + 1)]
 
 lastCol :: [String] -> Int
 lastCol board = (length $ board !! 0) - 1

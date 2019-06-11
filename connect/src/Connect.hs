d861caa0104bbc00861f0d21daed7774be4c5d43
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-11 22:46:34 +0200

    improved

--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -19,20 +19,20 @@ pathsFromStart board Cross  = [find board [] 'X' (r, 0) | r <- [0..(lastRow boar
 pathsFromStart board Nought = [find board [] 'O' (0, c) | c <- [0..(lastCol board)]]
 
 find :: [String] -> [(Int,Int)] -> Char -> (Int, Int) -> Maybe Mark
-find board checkedCells char cell =
-  let (row, col) = cell
-      outOfBounds board row col = row > lastRow board || col > lastCol board || row < 0 || col < 0
-      alreadyChecked = elem
-      noMatch board row col = (/=) (board !! row !! col)
-  in
-    if outOfBounds board row col || alreadyChecked cell checkedCells || noMatch board row col char then Nothing
-    else if char == 'X' && col == lastCol board then Just Cross
-    else if char == 'O' && row == lastRow board then Just Nought
-    else
-      foldl (<|>) Nothing $
-        (find board (cell : checkedCells) char) <$>
-          [(row - 1, col), (row + 1, col), (row, col + 1), (row, col - 1), (row + 1, col - 1), (row - 1, col + 1)]
-
+find board checkedCells char cell
+  | outOfBounds board row col || alreadyChecked cell checkedCells || noMatch board row col char = Nothing
+  | char == 'X' && col == lastCol board = Just Cross
+  | char == 'O' && row == lastRow board = Just Nought
+  | otherwise =
+    foldl (<|>) Nothing $
+      (find board (cell : checkedCells) char) <$>
+        [(row - 1, col), (row + 1, col), (row, col + 1), (row, col - 1), (row + 1, col - 1), (row - 1, col + 1)]
+  where
+    (row, col) = cell
+    outOfBounds board row col = row > lastRow board || col > lastCol board || row < 0 || col < 0
+    alreadyChecked = elem
+    noMatch board row col = (/=) (board !! row !! col)
+ 
 lastCol :: [String] -> Int
 lastCol board = (length $ board !! 0) - 1
 

5902dca23dd90c468fc194daadc5a4b16b1a5c82
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-15 13:50:29 +0200

    improved

--- connect/src/Connect.hs
+++ connect/src/Connect.hs
@@ -9,7 +9,7 @@ data Mark =
 
 winner :: [String] -> Maybe Mark
 winner board =
-  foldl (<|>) Nothing $ (spot board) <$> [Cross, Nought] 
+  asum $ (spot board) <$> [Cross, Nought] 
 
 spot :: [String] -> Mark -> Maybe Mark
 spot board mark =
@@ -21,21 +21,20 @@ pathsFromStart board Nought = [find board [] 'O' (0, c) | c <- [0..(lastCol boar
 
 find :: [String] -> [(Int,Int)] -> Char -> (Int, Int) -> Maybe Mark
 find board checkedCells char cell@(row, col)
-  | outOfBounds board row col = Nothing
-  | alreadyChecked cell checkedCells = Nothing
-  | noMatch board row col char = Nothing
+  | outOfBounds board row col           = Nothing
+  | alreadyChecked cell checkedCells    = Nothing
+  | noMatch board row col char          = Nothing
   | char == 'X' && col == lastCol board = Just Cross
   | char == 'O' && row == lastRow board = Just Nought
-  | otherwise =
-    foldl (<|>) Nothing $
-      (find board (cell : checkedCells) char) <$> [
-        (row - 1, col)
-      , (row + 1, col)
-      , (row, col + 1)
-      , (row, col - 1)
-      , (row + 1, col - 1)
-      , (row - 1, col + 1)
-    ]
+  | otherwise = asum $
+    (find board (cell : checkedCells) char) <$> [
+      (row - 1, col)
+    , (row + 1, col)
+    , (row, col + 1)
+    , (row, col - 1)
+    , (row + 1, col - 1)
+    , (row - 1, col + 1)
+  ]
   where
     outOfBounds board row col = row > lastRow board || col > lastCol board || row < 0 || col < 0
     alreadyChecked = elem

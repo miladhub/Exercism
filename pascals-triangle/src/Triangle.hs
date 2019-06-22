f44705a2a6dd548b0079c60ed26120736b29aa63
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-22 21:33:14 +0200

    using iterate

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -2,12 +2,10 @@ module Triangle (rows) where
 
 rows :: Int -> [[Int]]
 rows 0 = []
-rows 1 = [[1]]
-rows n =
-  let previous = rows $ n - 1
-  in previous ++ [next (head $ reverse previous)]
+rows n = take n $ iterate next [1]
 
 next :: [Int] -> [Int]
 next r =
   let padded = 0 : r ++ [0]
-  in fmap (uncurry (+)) $ zip padded (drop 1 padded)
+      pairs = zip padded $ drop 1 padded
+  in fmap (uncurry (+)) pairs

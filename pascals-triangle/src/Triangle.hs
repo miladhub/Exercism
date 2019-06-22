feb42410a9f46d16d6a93ca14d0f4c8dd70ee4b0
Milad Bourhani <milad.bourhani@gmail.com>
2019-06-22 21:12:27 +0200

    first working version

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,4 +1,13 @@
 module Triangle (rows) where
 
-rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows :: Int -> [[Int]]
+rows 0 = []
+rows 1 = [[1]]
+rows n =
+  let previous = rows $ n - 1
+  in previous ++ [next (head $ reverse previous)]
+
+next :: [Int] -> [Int]
+next r =
+  let padded = 0 : r ++ [0]
+  in fmap (uncurry (+)) $ zip padded (drop 1 padded)

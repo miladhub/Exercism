34a5e3532e02030c45d0f5e79bf4f12fcbdacb38
milad.bourhani <milad.bourhani@accenture.com>
2020-03-06 09:01:31 +0100

    improved

--- dominoes/src/Dominoes.hs
+++ dominoes/src/Dominoes.hs
@@ -1,28 +1,32 @@
 module Dominoes (chain) where
 
 import Data.Tuple (swap)
-import Data.List (permutations)
-import Control.Applicative (liftA2)
 
 chain :: [(Int, Int)] -> Maybe [(Int, Int)]
-chain d =
-  let perms = permutations d >>= mapM (\e -> [e, swap e])
-      matches = filter (liftA2 (&&) connected looped) perms
-  in
-    if null matches then
-      Nothing
-    else
-      Just (head matches)
+chain []    = Just []
+chain (h:t) =
+  let s = filter loops $ search h t
+  in if null s then Nothing
+  else Just (head s)
 
-connected :: [(Int, Int)] -> Bool
-connected (f : (s : t)) 
-  | connects f s = connected (s : t)
-  | otherwise    = False
-connected _ = True
+search :: (Int,Int) -> [(Int,Int)] -> [[(Int,Int)]]
+search d []  = [[d]]
+search d bag =
+  map (d:) $ concat [ search candidate (bag `minus` i)
+                    | i <- [0..(length bag - 1)],
+                      let d' = bag !! i
+                    , candidate <- [d', swap d'] 
+                    , conn d candidate
+                    ]
 
-looped :: [(Int, Int)] -> Bool
-looped []  = True
-looped l   = connects (last l) (head l)
+minus :: [a] -> Int -> [a]
+minus b i =
+  let s = splitAt i b
+  in fst s ++ tail (snd s)
 
-connects :: (Int, Int) -> (Int, Int) -> Bool
-connects a b = snd a == fst b
+loops :: [(Int, Int)] -> Bool
+loops l = conn (last l) (head l)
+
+conn :: (Int, Int) -> (Int, Int) -> Bool
+conn f s  = snd f == fst s
+

3a873a129c1623d5ceae73aa5adaff27f648aed9
milad.bourhani <milad.bourhani@accenture.com>
2020-03-07 14:28:24 +0100

    first impl

--- bank-account/src/BankAccount.hs
+++ bank-account/src/BankAccount.hs
@@ -6,16 +6,40 @@ module BankAccount
     , openAccount
     ) where
 
-data BankAccount = Dummy
+import Control.Monad.STM (atomically)
+import Control.Concurrent.STM (TVar, newTVar, readTVar, writeTVar)
+
+data Account = Account
+  {
+    balance :: Integer
+  , open :: Bool
+  }
+
+type BankAccount = TVar Account
 
 closeAccount :: BankAccount -> IO ()
-closeAccount account = error "You need to implement this function."
+closeAccount account = atomically $ do
+  acc <- readTVar account
+  writeTVar account (acc {open = False})
 
 getBalance :: BankAccount -> IO (Maybe Integer)
-getBalance account = error "You need to implement this function."
+getBalance account = atomically $ do
+  acc <- readTVar account
+  return $
+    if open acc then Just (balance acc)
+    else Nothing
 
 incrementBalance :: BankAccount -> Integer -> IO (Maybe Integer)
-incrementBalance account amount = error "You need to implement this function."
+incrementBalance account amount = atomically $ do
+  acc <- readTVar account
+  if open acc then
+    let new = amount + balance acc
+    in do
+      writeTVar account (acc {balance = new})
+      return (Just new)
+  else
+    return Nothing
 
 openAccount :: IO BankAccount
-openAccount = error "You need to implement this function."
+openAccount = atomically (newTVar $ Account 0 True)
+

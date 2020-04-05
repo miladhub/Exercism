4cd1874919617fbfc23b3197d9e415639d47d078
Milad <milad.bourhani@gmail.com>
2020-04-05 23:20:55 +0200

    first version

--- simple-cipher/src/Cipher.hs
+++ simple-cipher/src/Cipher.hs
@@ -1,10 +1,36 @@
 module Cipher (caesarDecode, caesarEncode, caesarEncodeRandom) where
 
+import Data.Char (ord, chr)
+import System.Random (getStdRandom,randomR)
+import Control.Monad (replicateM)
+
 caesarDecode :: String -> String -> String
-caesarDecode key encodedText = error "You need to implement this function."
+caesarDecode key encodedText =
+  zipWith unshift (cycle key) encodedText
 
 caesarEncode :: String -> String -> String
-caesarEncode key text = error "You need to implement this function."
+caesarEncode key text =
+  zipWith shift (cycle key) text
+
+shift :: Char -> Char -> Char
+shift k c =
+  let d = ord k - ord 'a'
+      p = ord c - ord 'a'
+  in chr $ (ord 'a' + (p + d) `modAbs` 26)
+
+unshift :: Char -> Char -> Char
+unshift k c =
+  let d = ord k - ord 'a'
+      p = ord c - ord 'a'
+  in chr $ (ord 'a' + (p - d) `modAbs` 26)
+
+modAbs :: Int -> Int -> Int
+modAbs x m
+  | x >= 0    = x `mod` m
+  | otherwise = (m + x) `mod` m 
 
 caesarEncodeRandom :: String -> IO (String, String)
-caesarEncodeRandom text = error "You need to implement this function."
+caesarEncodeRandom text = do
+  let rand = getStdRandom (randomR (ord 'a', ord 'z'))
+  key <- (fmap . fmap) chr $ replicateM (length text) rand 
+  return $ (key, caesarEncode key text)
